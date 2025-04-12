473335bbcb0633813761f856be1dd0d3ae73635a
fabpaiprof <fabpaiprof@gmail.com>
2025-04-12 18:12:54 +0000

    Stampa nome VERTICALE  da VECTOR

--- Hello.java
+++ Hello.java
@@ -1,8 +1,20 @@
+import java.util.Vector;
+
 public class Hello {
 
     public static void main(String[] args) {
  
        System.out.println("Hello Fabio !");
- 
+       
+       Vector<Character> lettere = new Vector<>();
+       lettere.add('f');
+       lettere.add('a');
+       lettere.add('b');
+       lettere.add('i');
+       lettere.add('o');
+   
+       for (Character lettera : lettere) {
+           System.out.println(lettera);
+       }
     }
  }
