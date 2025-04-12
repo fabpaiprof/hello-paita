36f467f349746d13916696c9a5a8311699f8f27e
fabpaiprof <fabpaiprof@gmail.com>
2025-04-12 17:54:19 +0000

    Stampa nome ORIZZONTALE da ARRAY

--- Hello.java
+++ Hello.java
@@ -3,6 +3,13 @@ public class Hello {
     public static void main(String[] args) {
  
        System.out.println("Hello Fabio !");
- 
-    }
+
+      // Array di caratteri con le lettere del nome "fabio"
+      char[] lettere = {'f', 'a', 'b', 'i', 'o'};
+
+      // Stampa le lettere dell'array
+      for (char lettera : lettere) {
+         System.out.print(lettera + " ");
+      }
+    } 
  }
